de1ed0e74517d8a4a70237cde839e2077a9cb856
Kkuntal990 <kokatekuntal@gmail.com>
2021-09-27 17:13:12 +0530

    Added dataset

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,11 +3,11 @@ import pandas as pd
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--path', help='Path to the dataset to be loaded', default='dataset/test9_time.csv')
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-p', '--path', help='Path to the dataset to be loaded', default='dataset/test9_time.csv')
 
-args = vars(parser.parse_args())
-PATH = args['path']
+# args = vars(parser.parse_args())
+# PATH = args['path']
 
 def convert_list_string_to_numpy_int_array(data):
     #data = [for x in data ]
@@ -21,6 +21,13 @@ def load_data(PATH):
     data = pd.read_csv(PATH)
     return data
 
+def load_CSI(PATH):
+    data = pd.read_csv(PATH)
+    data = data['CSI_DATA']
+    data = [x[1:-1] for x in data]
+    data = [list(map(np.int64, x.strip().split())) for x in data]
+    data = np.array(data)
+    return data
 
 if __name__ == '__main__':
     data = load_data(PATH)
